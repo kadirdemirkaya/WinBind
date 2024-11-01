4a339e1f5707ae7d5b7ae52a0a9e5f51b91effbe
GitLessonConfig <126807887+kadirdemirkaya@users.noreply.github.com>
2024-11-01 21:52:59 +0300

    api di getted

--- Presentation/WinBind.Api/Program.cs
+++ Presentation/WinBind.Api/Program.cs
@@ -1,20 +1,29 @@
+using WinBind.Application;
+using WinBind.Infrastructure;
+using WinBind.Persistence;
+
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
-
 builder.Services.AddControllers();
-// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+
 builder.Services.AddEndpointsApiExplorer();
+
 builder.Services.AddSwaggerGen();
+
+builder.Services.AddHttpContextAccessor();
+
+builder.Services.ApplicationServiceRegistration();
+
+builder.Services.InfrastructureServiceRegistration();
+
+builder.Services.PersistenceServiceRegistration(builder.Configuration);
+
 
 var app = builder.Build();
 
-// Configure the HTTP request pipeline.
-if (app.Environment.IsDevelopment())
-{
-    app.UseSwagger();
-    app.UseSwaggerUI();
-}
+app.UseSwagger();
+
+app.UseSwaggerUI();
 
 app.UseHttpsRedirection();
 
