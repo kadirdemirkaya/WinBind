61ccc400e2999a1a2f04217e2d70547d8fa0f53d
GitLessonConfig <126807887+kadirdemirkaya@users.noreply.github.com>
2024-11-05 19:02:16 +0300

    basic pagination service and midd improved

--- Infrastructure/WinBind.Infrastructure/DependencyInjection.cs
+++ Infrastructure/WinBind.Infrastructure/DependencyInjection.cs
@@ -1,5 +1,7 @@
-﻿using Microsoft.Extensions.DependencyInjection;
+﻿using Microsoft.AspNetCore.Builder;
+using Microsoft.Extensions.DependencyInjection;
 using WinBind.Application.Abstractions;
+using WinBind.Infrastructure.Middlewares;
 using WinBind.Infrastructure.Services;
 
 namespace WinBind.Infrastructure
@@ -10,7 +12,16 @@ namespace WinBind.Infrastructure
         {
             services.AddScoped<ITokenService, TokenService>();
 
+            services.AddScoped<IPaginationService, PaginationService>();
+
             return services;
+        }
+
+        public static IApplicationBuilder InfrastructureApplicationBuilder(this IApplicationBuilder app)
+        {
+            app.UseMiddleware<PaginationMiddleware>();
+
+            return app;
         }
     }
 }
---
+++ Infrastructure/WinBind.Infrastructure/Services/PaginationService.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Http;
+using WinBind.Application.Abstractions;
+using WinBind.Domain.Models.Options;
+
+namespace WinBind.Infrastructure.Services
+{
+    public class PaginationService(IHttpContextAccessor _httpContextAccessor) : IPaginationService
+    {
+        public PaginationOptions GetPaginationOptions()
+            => _httpContextAccessor.HttpContext.Items["PaginationOptions"] as PaginationOptions;
+    }
+}
