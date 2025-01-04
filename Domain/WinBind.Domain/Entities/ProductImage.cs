a5cbb1a213179aab4e843462cf275b44884d067c
GitLessonConfig <126807887+kadirdemirkaya@users.noreply.github.com>
2025-01-04 22:10:32 +0300

    image tablşe property updated

--- Domain/WinBind.Domain/Entities/ProductImage.cs
+++ Domain/WinBind.Domain/Entities/ProductImage.cs
@@ -4,11 +4,9 @@ namespace WinBind.Domain.Entities
 {
     public class ProductImage : BaseEntity
     {
-        public byte[] ImageData { get; set; }
-        public string ImageName { get; set; }
-        public string MimeType { get; set; }
-        public Guid ProductId { get; set; }
+        public string Path { get; set; }
 
+        public Guid ProductId { get; set; }
         public virtual Product Product { get; set; }
     }
 }
