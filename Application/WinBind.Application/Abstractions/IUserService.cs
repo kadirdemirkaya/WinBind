5d0cd656c12fa0626b728b248fae9feed749f43b
GitLessonConfig <126807887+kadirdemirkaya@users.noreply.github.com>
2025-01-05 14:43:30 +0300

    uyser service and mapper developed

---
+++ Application/WinBind.Application/Abstractions/IUserService.cs
@@ -0,0 +1,12 @@
+﻿using WinBind.Application.Features.Commands.Requests;
+using WinBind.Domain.Models.User;
+
+namespace WinBind.Application.Abstractions
+{
+    public interface IUserServive
+    {
+        Task<UserRegisterModel> UserRegisterAsync(UserRegisterCommandRequest userRegisterCommand, string? role = "User");
+
+        Task<UserLoginModel> UserLoginAsync(UserLoginCommandRequest userLoginCommand);
+    }
+}
