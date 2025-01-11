a1928778ab5bcc1d0479e019a90a32002c495332
GitLessonConfig <126807887+kadirdemirkaya@users.noreply.github.com>
2025-01-11 21:22:39 +0300

    basket controller added

--- Presentation/WinBind.Api/Program.cs
+++ Presentation/WinBind.Api/Program.cs
@@ -1,3 +1,4 @@
+using Microsoft.OpenApi.Models;
 using WinBind.Application;
 using WinBind.Infrastructure;
 using WinBind.Persistence;
@@ -8,6 +9,8 @@ builder.Services.AddControllers();
 
 builder.Services.AddEndpointsApiExplorer();
 
+builder.Services.AddHttpContextAccessor();
+
 builder.Services.AddRouting(opt =>
 {
     opt.LowercaseUrls = true;
@@ -15,9 +18,35 @@ builder.Services.AddRouting(opt =>
 
 builder.Services.AddCors();
 
-builder.Services.AddSwaggerGen();
+builder.Services.AddSwaggerGen(c =>
+{
+    c.SwaggerDoc("v1", new OpenApiInfo { Title = "WinBid", Version = "v1" });
 
-builder.Services.AddHttpContextAccessor();
+    c.AddSecurityDefinition("Bearer", new OpenApiSecurityScheme
+    {
+        Name = "Authorization",
+        Type = SecuritySchemeType.Http,
+        Scheme = "bearer",
+        BearerFormat = "JWT",
+        In = ParameterLocation.Header,
+        Description = "JWT Authorization header using the Bearer scheme. Example: 'Bearer {token}'"
+    });
+
+    c.AddSecurityRequirement(new OpenApiSecurityRequirement
+    {
+        {
+            new OpenApiSecurityScheme
+            {
+                Reference = new OpenApiReference
+                {
+                    Type = ReferenceType.SecurityScheme,
+                    Id = "Bearer"
+                }
+            },
+            new string[] {}
+        }
+    });
+});
 
 builder.Services.ApplicationServiceRegistration();
 
@@ -37,9 +66,13 @@ app.UseSwagger();
 
 app.UseSwaggerUI();
 
+app.PersistenceApplicationBuilderRegistration();
+
 app.InfrastructureApplicationBuilder();
 
 app.UseHttpsRedirection();
+
+app.UseAuthentication();
 
 app.UseAuthorization();
 
