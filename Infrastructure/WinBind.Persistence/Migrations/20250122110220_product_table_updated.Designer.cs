198b7b072f565b3b0d9b5f17a755b24aa8d63c46
GitLessonConfig <126807887+kadirdemirkaya@users.noreply.github.com>
2025-01-22 14:04:34 +0300

    migrations added

--- Infrastructure/WinBind.Persistence/Migrations/20250122110220_product_table_updated.Designer.cs
+++ Infrastructure/WinBind.Persistence/Migrations/20250122110220_product_table_updated.Designer.cs
@@ -2,6 +2,7 @@
 using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
 using WinBind.Persistence.Data;
@@ -11,9 +12,11 @@ using WinBind.Persistence.Data;
 namespace WinBind.Persistence.Migrations
 {
     [DbContext(typeof(WinBindDbContext))]
-    partial class WinBindDbContextModelSnapshot : ModelSnapshot
+    [Migration("20250122110220_product_table_updated")]
+    partial class product_table_updated
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
---
+++ Infrastructure/WinBind.Persistence/Migrations/20250122110220_product_table_updated.cs
@@ -0,0 +1,106 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace WinBind.Persistence.Migrations
+{
+    /// <inheritdoc />
+    public partial class product_table_updated : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "BandColor",
+                table: "Products",
+                type: "text",
+                nullable: false,
+                defaultValue: "");
+
+            migrationBuilder.AddColumn<string>(
+                name: "Brand",
+                table: "Products",
+                type: "text",
+                nullable: false,
+                defaultValue: "");
+
+            migrationBuilder.AddColumn<string>(
+                name: "CaseColor",
+                table: "Products",
+                type: "text",
+                nullable: false,
+                defaultValue: "");
+
+            migrationBuilder.AddColumn<string>(
+                name: "CaseShape",
+                table: "Products",
+                type: "text",
+                nullable: false,
+                defaultValue: "");
+
+            migrationBuilder.AddColumn<string>(
+                name: "DialColor",
+                table: "Products",
+                type: "text",
+                nullable: false,
+                defaultValue: "");
+
+            migrationBuilder.AddColumn<string>(
+                name: "Gender",
+                table: "Products",
+                type: "text",
+                nullable: false,
+                defaultValue: "");
+
+            migrationBuilder.AddColumn<string>(
+                name: "Model",
+                table: "Products",
+                type: "text",
+                nullable: false,
+                defaultValue: "");
+
+            migrationBuilder.AddColumn<string>(
+                name: "Technology",
+                table: "Products",
+                type: "text",
+                nullable: false,
+                defaultValue: "");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "BandColor",
+                table: "Products");
+
+            migrationBuilder.DropColumn(
+                name: "Brand",
+                table: "Products");
+
+            migrationBuilder.DropColumn(
+                name: "CaseColor",
+                table: "Products");
+
+            migrationBuilder.DropColumn(
+                name: "CaseShape",
+                table: "Products");
+
+            migrationBuilder.DropColumn(
+                name: "DialColor",
+                table: "Products");
+
+            migrationBuilder.DropColumn(
+                name: "Gender",
+                table: "Products");
+
+            migrationBuilder.DropColumn(
+                name: "Model",
+                table: "Products");
+
+            migrationBuilder.DropColumn(
+                name: "Technology",
+                table: "Products");
+        }
+    }
+}
